e58c6e47cf1a7be433bddab0ebcf053923c1d4ef
rgnrok <rgnrok91@gmail.com>
2022-06-27 17:17:28 +0300

    Add package submodule

---
+++ Game/Scripts/AssetsConstants.cs
@@ -0,0 +1,20 @@
+﻿namespace CharacterEditor
+{
+    public static class AssetsConstants
+    {
+        public const string CharacterEditorRootPath = "Assets/Packages/Character_Editor";
+        public static readonly string MeshPathTemplate = $"{CharacterEditorRootPath}/Prefabs/{{0}}/{{1}}";
+        public static readonly string SkinPathTemplate = $"{CharacterEditorRootPath}/Textures/Character/{{0}}/Skin/{{1}}";
+        public static readonly string ClothePathTemplate = $"{CharacterEditorRootPath}/Textures/Character/{{0}}/Clothes/{{1}}";
+
+        public static readonly string CharacterStaticDataPath = $"{CharacterEditorRootPath}/Data";
+
+        public const string GameRootPath = "Assets/Game";
+        public static readonly string GameStaticDataPath = $"{GameRootPath}/Data";
+
+        public static readonly string ArmorMergeMaterialPathKey = "ArmorMergeMaterialPathKey";
+        public static readonly string ClothMergeMaterialPathKey = "ClothMergeMaterialPathKey";
+        public static readonly string ModelMaterialPathKey = "ModelMaterialPathKey";
+        public static readonly string PreviewMaterialPathKey = "PreviewMaterialPathKey";
+    }
+}
